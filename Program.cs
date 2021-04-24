31852cf126adbbcce22c98473c92d0e7c6bbfb74
Chinnapat062 <78645438+Chinnapat062@users.noreply.github.com>
2021-04-24 15:42:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ข้อสอบข้อ_6
+{
+    class Program
+    {
+        static void Stair(int n,  char alp)
+        {
+
+            for (int i = 1; i <= n; i++)
+            {
+                int k;
+
+                if (i % 2 != 0)
+                    k = i + 1;
+                else
+                    k = i;
+
+                for (int j = 0; j < k; j++)
+                    Console.Write("{0}",alp);
+                Console.WriteLine();
+            }
+        }
+         static void Main()
+        {
+            char alp;
+            alp = char.Parse(Console.ReadLine());
+            int n ;
+            n = int.Parse(Console.ReadLine());
+
+            Stair(n, alp);
+        }
+    }
+}
